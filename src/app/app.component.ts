a54a5925625badc76b6d91ccdb4e3642ba3ad50a
44r0n <aaron.sn.1988@gmail.com>
2018-10-02 21:19:43 +0200

    Add generic table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  columns = ['Name', 'Level', 'City'];
+  heroes = [{ name: "Hero name", level: 3, city: 'Murcia' },
+            { name: "Second hero name", level: 4, city: 'Petrer'}
+          ];
+  attNames = ['name', 'level', 'city'];
+
+  showElement(element: any): void {
+    alert(element.name);
+  }
 }
